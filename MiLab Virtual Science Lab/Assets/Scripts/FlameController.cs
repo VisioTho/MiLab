195a1efa891fb61b6b23168dd9c0856adbb3c515
VisioTho <84463094+VisioTho@users.noreply.github.com>
2021-07-13 17:59:47 -0400

    new files

---
+++ MiLab Virtual Science Lab/Assets/Scripts/FlameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameController : MonoBehaviour
+{
+    public ParticleSystem flame;
+    public void adjustFlame(float val)
+    {
+        var main = flame.main;
+        main.startLifetime = val;
+    }
+}
---
+++ MiLab Virtual Science Lab/Assets/Scripts/ItemDropHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class ItemDropHandler : MonoBehaviour, IDropHandler
+{
+    // reference to flame particle system
+    public ParticleSystem flame;
+
+    
+    void Start()
+    {
+        //no emission of flame particles on wake
+        flame.Stop();
+    }
+
+    /* start emitting after flint is dragged over bunsen burner
+    */
+    public void OnDrop(PointerEventData data)
+    {
+        if (data.pointerDrag != null)
+        {
+            Debug.Log ("Dropped object was: "  + data.pointerDrag);
+            if(data.pointerDrag.name == "flint")
+            {
+                Debug.Log("hhh");
+                flame.Play();
+            }
+        }
+    }
+}
