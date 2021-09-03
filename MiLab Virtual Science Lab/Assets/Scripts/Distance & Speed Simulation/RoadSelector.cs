8998973d1736263e83decf986df30036a2fa4e39
VisioTho <84463094+VisioTho@users.noreply.github.com>
2021-09-03 09:08:58 -0400

    sdd

--- MiLab Virtual Science Lab/Assets/Scripts/Distance & Speed Simulation/RoadSelector.cs
+++ MiLab Virtual Science Lab/Assets/Scripts/Distance & Speed Simulation/RoadSelector.cs
@@ -12,9 +12,13 @@ public class RoadSelector : MonoBehaviour
             flatRoad.SetActive(true);
             hillyRoad.SetActive(false);
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
             finishLineFlag.transform.position = new Vector2(35f, -5.6f);
 >>>>>>> parent of dc69827 (Resized the canvas scaler to 1640 x 720 Pixels for each scene)
+=======
+            finishLineFlag.transform.position = new Vector3(29.3999996f, -5.0999999f, 0);
+>>>>>>> parent of 6065926 (Revert "Resized the canvas scaler to 1640 x 720 Pixels for each scene")
         }
         if(val == 1)
         {
