9706202cf51a585fe55eb85b89630a6cd379d442
VisioTho <84463094+VisioTho@users.noreply.github.com>
2021-09-21 16:53:01 +0200

    New Multimeter scene + improvements to Bunsen Burner scene
    Bunsen burner now visually shows air holes closing when the user adjusts the slider  - used a swap sprite script  in flameController.cs
    
    New multimeter scene created. Includes a custom multimeter sprite(s) Only functionality as of now is adjusting the dial

---
+++ MiLab Virtual Science Lab/Assets/MiLab/Scripts/MultimeterSceneScripts/DialManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialManager : MonoBehaviour
+{
+    public SpriteRenderer multimeterSprite;
+    public Sprite[] multimeterSprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void swapSrites(int n)
+    {
+        multimeterSprite.sprite = multimeterSprites[n];
+    }
+}
