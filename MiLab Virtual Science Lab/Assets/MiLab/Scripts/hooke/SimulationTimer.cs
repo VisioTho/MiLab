f17ab2a4966c22f718615d5af98b24251c79938f
unknown <shawnthondoya@gmail.com>
2021-11-17 08:35:03 +0200

    Further logs

---
+++ MiLab Virtual Science Lab/Assets/MiLab/Scripts/hooke/SimulationTimer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SimulationTimer : MonoBehaviour
+{
+    float totalSimulationTime;
+    float StartTime;
+    public TMP_Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalSimulationTime = Time.time - StartTime;
+        float minutes = (int)totalSimulationTime / 60;
+        float seconds = totalSimulationTime % 60;
+        timerText.text = minutes.ToString("F0") + ":" +seconds.ToString("F0");
+    }
+}
