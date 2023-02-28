3985bb6053227a9ef5f5ee83f22523130eace256
Vision Thondoya <84463094+VisioTho@users.noreply.github.com>
2023-02-28 16:28:37 +0200

    Bubble bug fixed

--- MiLab Virtual Science Lab/Assets/MiLab/Scenes/Potometer/Scripts/BeakerController.cs
+++ MiLab Virtual Science Lab/Assets/MiLab/Scenes/Potometer/Scripts/BeakerController.cs
@@ -6,6 +6,7 @@ public class BeakerController : MonoBehaviour
 {
     public bool isConnectedToTube;
     private bool hasExited;
+    public GameObject potometer;
 
     // register that the beaker has been moved from its position on the potometer
     private void OnCollisionExit2D(Collision2D collision)
@@ -13,27 +14,56 @@ public class BeakerController : MonoBehaviour
         if (collision.gameObject.name == "BeakerArea")
         {
             hasExited = true;
+            hasCollided = false;
         }
     }
 
-    // register that the beaker has been placed in its position on the potometer
+    private void OnMouseDown()
+    {
+        potometer.LeanMoveLocalY(1.0f, 1.0f);
+        isFinishedMoving = false;
+    }
+    private void OnMouseUp()
+    {
+        potometer.LeanMoveLocalY(0.47f, 1.0f).setOnComplete(SetPotometerMovementComplete);
+    }
 
+    bool isFinishedMoving;
+    // register that the beaker has been placed in its position on the potometer
+    private void SetPotometerMovementComplete()
+    {
+        isFinishedMoving = true;
+    }
+    bool hasCollided;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+        
         if(collision.gameObject.name == "BeakerArea")
         {
-            if(hasExited)
-            {
-                isConnectedToTube = true;
-                hasExited = false;
-            }
+            hasCollided = true;
         }
     }
 
     private void Update()
     {
+        if(hasCollided)
+        {
+
+            if(hasExited)
+            {
+                if(isFinishedMoving)
+                {
+                    Debug.Log("apappa");
+                    isConnectedToTube = true;
+                    hasExited = false;
+                }
+                
+            }
+        }
+        
         BubbleController.InitiateBubbleMovement(this);
-        Debug.Log("state is " + isConnectedToTube);
+        Debug.Log("is completed" + isFinishedMoving);
+        Debug.Log("has collided" + hasCollided);
+        Debug.Log("has exited" + hasExited);
     }
 }
